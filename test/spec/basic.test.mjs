c7037e051895007167abea333a385b28a59c92d9
Nolan Lawson <nolan@nolanlawson.com>
2021-12-07 20:58:18 -0800

    test: more tests

--- test/spec/basic.test.mjs
+++ test/spec/basic.test.mjs
@@ -3,18 +3,20 @@ import { expect } from 'chai'
 
 describe('basic test suite', () => {
   it('can detect a simple leak', async () => {
-    const results = await findLeaks('http://localhost:3000/test/www/basic/')
+    const results = await findLeaks('http://localhost:3000/test/www/basic/', {
+      iterations: 3
+    })
 
     expect(results.length).to.equal(3)
-    expect(results.map(_ => _.test.data)).to.deep.equal([
-      { href: 'http://localhost:3000/test/www/basic/about' },
-      { href: 'http://localhost:3000/test/www/basic/info' },
-      { href: 'http://localhost:3000/test/www/basic/contact' }
+    expect(results.map(_ => ({ href: _.test.data.href } ))).to.deep.equal([
+      { href: 'about' },
+      { href: 'info' },
+      { href: 'contact' }
     ])
 
-    const deltas = results.map(_ => _.result.delta)
-    expect(deltas[0]).to.be.above(7000000)
-    expect(deltas[0]).to.be.below(8000000)
+    const deltas = results.map(_ => _.result.deltaPerIteration)
+    expect(deltas[0]).to.be.above(1000000)
+    expect(deltas[0]).to.be.below(2000000)
 
     expect(deltas[1]).to.be.above(0)
     expect(deltas[1]).to.be.below(100000)
@@ -23,7 +25,30 @@ describe('basic test suite', () => {
     expect(deltas[2]).to.be.below(100000)
 
     const leak = results[0].result.leakingObjects.find(_ => _.name === 'SomeBigObject')
-    expect(leak.retainedSizeDelta).to.be.above(7000000)
-    expect(leak.retainedSizeDelta).to.be.below(8000000)
+    expect(leak.retainedSizeDeltaPerIteration).to.be.above(1000000)
+    expect(leak.retainedSizeDeltaPerIteration).to.be.below(2000000)
+  })
+
+  it('works with invisible links', async () => {
+    const results = await findLeaks('http://localhost:3000/test/www/invisibleLinks/', {
+      iterations: 3
+    })
+
+    expect(results.length).to.equal(3)
+    expect(results.map(_ => ({ href: _.test.data.href } ))).to.deep.equal([
+      { href: 'about' },
+      { href: 'info' },
+      { href: 'contact' }
+    ])
+
+    const deltas = results.map(_ => _.result.deltaPerIteration)
+    expect(deltas[0]).to.be.above(0)
+    expect(deltas[0]).to.be.below(100000)
+
+    expect(deltas[1]).to.be.above(0)
+    expect(deltas[1]).to.be.below(100000)
+
+    expect(deltas[2]).to.be.above(0)
+    expect(deltas[2]).to.be.below(100000)
   })
 })
