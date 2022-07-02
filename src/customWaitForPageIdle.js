fdcf718432882f815dbac72b65f6c2b2f3fc0731
Nolan Lawson <nolan@nolanlawson.com>
2022-07-02 09:32:14 -0700

    fix: lint

--- src/customWaitForPageIdle.js
+++ src/customWaitForPageIdle.js
@@ -1,10 +1,10 @@
 // Set the custom idle logic globally so it's accessible in defaultScenario.js
 let customWaitForPageIdle
 
-export function setCustomWaitForPageIdle(waitForPageIdle) {
+export function setCustomWaitForPageIdle (waitForPageIdle) {
   customWaitForPageIdle = waitForPageIdle
 }
 
-export function getCustomWaitForPageIdle() {
+export function getCustomWaitForPageIdle () {
   return customWaitForPageIdle
 }
