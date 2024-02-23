9e2a772cec435b244bcdaa4a4473cd2811bae5c7
Nolan Lawson <nolan@nolanlawson.com>
2024-02-22 17:41:46 -0800

    test: safer password logic

--- test/spec/customScenario.test.mjs
+++ test/spec/customScenario.test.mjs
@@ -3,13 +3,15 @@ import * as defaultScenario from '../../src/defaultScenario.js'
 import { expect } from 'chai'
 import { asyncIterableToArray } from './util.js'
 
+// This password has to be random or else Chrome will pop up a "you have an unsafe password" modal
+const randoPassword = () => Array(5).fill().map(() => Math.floor(Math.random() * 1000000).toString(16)).join('-')
+
 describe('custom scenario', () => {
   it('can do a custom version of the default scenario', async () => {
     const scenario = {
       async setup (page) {
         await (await page.$('#username')).type('myusername')
-        // This password has to be random or else Chrome will pop up a "you have an unsafe password" modal
-        await (await page.$('#password')).type(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16))
+        await (await page.$('#password')).type(randoPassword())
         await (await page.$('#submit')).click()
       },
       createTests: defaultScenario.createTests,
