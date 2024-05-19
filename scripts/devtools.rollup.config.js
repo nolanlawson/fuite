56b4bbfc3f471d6cdbf1c56671240c5f81f36030
Nolan Lawson <nolan@nolanlawson.com>
2024-05-18 17:52:31 -0700

    fix: lint

--- scripts/devtools.rollup.config.js
+++ scripts/devtools.rollup.config.js
@@ -47,7 +47,7 @@ export default {
     }),
     typescript({
       compilerOptions: {
-        lib: ['esnext', 'dom', "dom.iterable", 'webworker', 'webworker.iterable'],
+        lib: ['esnext', 'dom', 'dom.iterable', 'webworker', 'webworker.iterable'],
         target: 'esnext'
       },
       noEmitOnError: false
