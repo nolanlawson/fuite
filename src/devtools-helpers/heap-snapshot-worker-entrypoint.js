dc4bc6008bd2c3bb3e6fd733c9d3578669d7f125
Nolan Lawson <nolan@nolanlawson.com>
2025-04-12 15:58:03 -0700

    Revert "try web-worker, nope"
    This reverts commit 74cd49aafffac43996c778685d82e2f1b96e6acf.

--- src/devtools-helpers/heap-snapshot-worker-entrypoint.js
+++ src/devtools-helpers/heap-snapshot-worker-entrypoint.js
@@ -1,9 +1,7 @@
-import 'web-worker'
+import { parentPort } from 'node:worker_threads'
 import { HeapSnapshotWorker } from '../thirdparty/devtools-frontend/index.js'
 
-/* global self */
-
 const dispatcher =
-  new HeapSnapshotWorker.HeapSnapshotWorkerDispatcher.HeapSnapshotWorkerDispatcher(self.postMessage.bind(self))
-self.addEventListener('message', dispatcher.dispatchMessage.bind(dispatcher), false)
-self.postMessage('workerReady')
+  new HeapSnapshotWorker.HeapSnapshotWorkerDispatcher.HeapSnapshotWorkerDispatcher(parentPort.postMessage.bind(parentPort))
+parentPort.on('message', dispatcher.dispatchMessage.bind(dispatcher))
+parentPort.postMessage('workerReady')
--- src/devtools-helpers/web-compatible-worker.js
+++ src/devtools-helpers/web-compatible-worker.js
@@ -1,5 +1,48 @@
 // wrapper around Node's Worker to make onmessage work
 
-import Worker from 'web-worker'
+import { Worker } from 'node:worker_threads'
 
-export { Worker as WebCompatibleWorker }
+export class WebCompatibleWorker {
+  #worker
+  #onMessage = null
+  #onError = null
+  constructor(...args) {
+    this.#worker = new Worker(...args)
+  }
+
+  get onmessage() {
+    return this.#onMessage
+  }
+  set onmessage(listener) {
+    if (listener === null) {
+      if (this.#onMessage) {
+        this.#worker.off('message', this.#onMessage)
+      }
+    } else {
+      this.#worker.on('message', listener)
+    }
+    this.#onMessage = null
+  }
+
+  get onerror() {
+    return this.#onError
+  }
+  set onerror(listener) {
+    if (listener === null) {
+      if (this.#onError) {
+        this.#worker.off('error', this.#onError)
+      }
+    } else {
+      this.#worker.on('error', listener)
+    }
+    this.#onError = null
+  }
+
+  postMessage(...args) {
+    this.#worker.postMessage(...args)
+  }
+
+  terminate() {
+    this.#worker.terminate()
+  }
+}
